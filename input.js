6ba576d0a5129b64713a33565a72b45410978f47
Nick <ndanvers222@gmail.com>
2020-01-16 00:37:58 +0000

    snake now works

--- input.js
+++ input.js
@@ -1,4 +1,8 @@
-const setupInput = function() {
+let connection;
+
+const setupInput = function(conn) {
+    console.log(conn);
+    connection = conn;
     const stdin = process.stdin;
     stdin.setRawMode(true);
     stdin.setEncoding('utf8');
@@ -7,6 +11,18 @@ const setupInput = function() {
         if (handleUserInput === '\u0003') {
             console.log("Thanks for using me, ciao!");
             process.exit();
+        }
+        if (handleUserInput === 'w'){
+            connection.write("Move: up");
+        }
+        if (handleUserInput === 'a'){
+            connection.write("Move: left");
+        }
+        if (handleUserInput === 's'){
+            connection.write("Move: down");
+        }
+        if (handleUserInput === 'd'){
+            connection.write("Move: right");
         }
     })
     return stdin;
