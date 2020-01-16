e0fffc729de250aebc175d473231a75b28de521b
Nick <ndanvers222@gmail.com>
2020-01-16 00:10:54 +0000

    .

---
+++ input.js
@@ -0,0 +1,15 @@
+const setupInput = function() {
+    const stdin = process.stdin;
+    stdin.setRawMode(true);
+    stdin.setEncoding('utf8');
+    stdin.resume();
+    stdin.on('data', (handleUserInput) => {
+        if (handleUserInput === '\u0003') {
+            console.log("Thanks for using me, ciao!");
+            process.exit();
+        }
+    })
+    return stdin;
+  }
+  setupInput();
+  module.exports = {setupInput};
--- play.js
+++ play.js
@@ -1,21 +1,8 @@
 // play.js
 const {connect} = require('./client');
-
-const setupInput = function() {
-    const stdin = process.stdin;
-    stdin.setRawMode(true);
-    stdin.setEncoding('utf8');
-    stdin.resume();
-    stdin.on('data', (handleUserInput) => {
-        if (handleUserInput === '\u0003') {
-            console.log("Thanks for using me, ciao!");
-            process.exit();
-        }
-    })
-    return stdin;
-  }
-
+const {setupInput} = require('./input')
 
 console.log('Connecting ...');
-setupInput();
 connect();
+
+setupInput();
